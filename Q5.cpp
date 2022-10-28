35285670c4c3ff702899bf6ebf0b5212db3b0a30
Talha_Iqbal <113589282+TalhaIqbalCh@users.noreply.github.com>
2022-10-28 00:50:03 -0700

    Update Q5.cpp

--- Q5.cpp
+++ Q5.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
 	//alphabets
-	int count=1;
+	int count = 1;
 	char a, b, c, d, e, f, g;
 
 	cout << "Input 7 characters: \n";
@@ -31,7 +31,28 @@ int main() {
 		count++;
 	}
 
-	cout << count << " " << a << " ";
+	if (count == 1) {
+		cout << "One " << a << " ";
+	}
+	else if (count == 2) {
+		cout << "Two " << a << " ";
+	}
+	else if (count == 3) {
+		cout << "Three " << a << " ";
+	}
+	else if (count == 4) {
+		cout << "Four " << a << " ";
+	}
+	else if (count == 5) {
+		cout << "Five " << a << " ";
+	}
+	else if (count == 6) {
+		cout << "Six " << a << " ";
+	}
+	else if (count == 7) {
+		cout << "Seven " << a << " ";
+	}
+
 
 	count = 1;
 
@@ -51,7 +72,27 @@ int main() {
 		if (b == g) {
 			count++;
 		}
-		cout << count << " " << b << " ";
+		if (count == 1) {
+			cout << "One " << b << " ";
+		}
+		else if (count == 2) {
+			cout << "Two " << b << " ";
+		}
+		else if (count == 3) {
+			cout << "Three " << b << " ";
+		}
+		else if (count == 4) {
+			cout << "Four " << b << " ";
+		}
+		else if (count == 5) {
+			cout << "Five " << b << " ";
+		}
+		else if (count == 6) {
+			cout << "Six " << b << " ";
+		}
+		else if (count == 7) {
+			cout << "Seven " << b << " ";
+		}
 	}
 
 	count = 1;
@@ -69,7 +110,27 @@ int main() {
 		if (c == g) {
 			count++;
 		}
-		cout << count << " " << c << " ";
+		if (count == 1) {
+			cout << "One " << c << " ";
+		}
+		else if (count == 2) {
+			cout << "Two " << c << " ";
+		}
+		else if (count == 3) {
+			cout << "Three " << c << " ";
+		}
+		else if (count == 4) {
+			cout << "Four " << c << " ";
+		}
+		else if (count == 5) {
+			cout << "Five " << c << " ";
+		}
+		else if (count == 6) {
+			cout << "Six " << c << " ";
+		}
+		else if (count == 7) {
+			cout << "Seven " << c << " ";
+		}
 	}
 
 	count = 1;
@@ -84,7 +145,27 @@ int main() {
 		if (d == g) {
 			count++;
 		}
-		cout << count << " " << d << " ";
+		if (count == 1) {
+			cout << "One " << d << " ";
+		}
+		else if (count == 2) {
+			cout << "Two " << d << " ";
+		}
+		else if (count == 3) {
+			cout << "Three " << d << " ";
+		}
+		else if (count == 4) {
+			cout << "Four " << d << " ";
+		}
+		else if (count == 5) {
+			cout << "Five " << d << " ";
+		}
+		else if (count == 6) {
+			cout << "Six " << d << " ";
+		}
+		else if (count == 7) {
+			cout << "Seven " << d << " ";
+		}
 	}
 
 	count = 1;
@@ -96,7 +177,27 @@ int main() {
 		if (e == g) {
 			count++;
 		}
-		cout << count << " " << e << " ";
+		if (count == 1) {
+			cout << "One " << e << " ";
+		}
+		else if (count == 2) {
+			cout << "Two " << e << " ";
+		}
+		else if (count == 3) {
+			cout << "Three " << e << " ";
+		}
+		else if (count == 4) {
+			cout << "Four " << e << " ";
+		}
+		else if (count == 5) {
+			cout << "Five " << e << " ";
+		}
+		else if (count == 6) {
+			cout << "Six " << e << " ";
+		}
+		else if (count == 7) {
+			cout << "Seven " << e << " ";
+		}
 	}
 
 	count = 1;
@@ -105,13 +206,53 @@ int main() {
 		if (f == g) {
 			count++;
 		}
-		cout << count << " " << f << " ";
+		if (count == 1) {
+			cout << "One " << f << " ";
+		}
+		else if (count == 2) {
+			cout << "Two " << f << " ";
+		}
+		else if (count == 3) {
+			cout << "Three " << f << " ";
+		}
+		else if (count == 4) {
+			cout << "Four " << f << " ";
+		}
+		else if (count == 5) {
+			cout << "Five " << f << " ";
+		}
+		else if (count == 6) {
+			cout << "Six " << f << " ";
+		}
+		else if (count == 7) {
+			cout << "Seven " << f << " ";
+		}
 	}
 
 	count = 1;
 
 	if (g != a && g != b && g != c && g != d && g != e && g != f) {
-		cout << count << " " << g << " ";
+		if (count == 1) {
+			cout << "One " << g << " ";
+		}
+		else if (count == 2) {
+			cout << "Two " << g << " ";
+		}
+		else if (count == 3) {
+			cout << "Three " << g << " ";
+		}
+		else if (count == 4) {
+			cout << "Four " << g << " ";
+		}
+		else if (count == 5) {
+			cout << "Five " << g << " ";
+		}
+		else if (count == 6) {
+			cout << "Six " << g << " ";
+		}
+		else if (count == 7) {
+			cout << "Seven " << g << " ";
+		}
 	}
 
 	return 0;
